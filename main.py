6d3811e021c0b9a358130d1213a5dfafc21460c2
unknown <arnauldkayonga1@gmail.com>
2022-09-25 01:07:04 +0200

    Added testing dir

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
 from packages import pindo
 
 app = FastAPI()
 
-
+#INDEX
 @app.get("/")
 def root():
     return {"Hello": "World"}
 
+#SPEECH ENABLED QUERY KINYARWANDA
+@app.post("/query/speech/rw")
+def query_voice_rw() -> dict:
+    return {"Hello": "World"}
+
+#SPEECH ENABLED QUERY ENGLISH
+@app.post("/query/speech/en")
+def query_voice_rw() -> dict:
+    return {"Hello": "World"}
+
+#TEXT BASED QUERY KINYARWANDA
+@app.post("/query/text/rw")
+def query_voice_rw(query: str) -> dict:
+    return {"Hello": "World"}
+
+#TEXT BASED QUERY ENGLISH
+@app.post("/query/text/en")
+def query_voice_rw(query: str) -> dict:
+    return {"Hello": "World"}
