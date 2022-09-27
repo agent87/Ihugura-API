48813ee4a7c092d379595c46a94b4e71151b4d11
unknown <arnauldkayonga1@gmail.com>
2022-09-27 10:39:53 +0200

    removed other docs included code of criminal procedure

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import time
 import os
 from haystack.document_stores import ElasticsearchDocumentStore
 from haystack.utils import clean_wiki_text, convert_files_to_docs
+from haystack.nodes import BM25Retriever
+from haystack.nodes import FARMReader
+from haystack.pipelines import ExtractiveQAPipeline
 
 try:
     with open('config.json') as config_file:
@@ -27,6 +30,30 @@ time.sleep(30)
 host = os.environ.get("ELASTICSEARCH_HOST", "localhost")
 document_store = ElasticsearchDocumentStore(host=host, username="", password="", index="document")
 
+
+
+docs = convert_files_to_docs(dir_path="docs/", clean_func=clean_wiki_text, split_paragraphs=True)
+
+
+print(docs[:3])
+
+# Now, let's write the dicts containing documents to our DB.
+document_store.write_documents(docs)
+
+
+# Load a  local model or any of the QA models on
+# Hugging Face's model hub (https://huggingface.co/models)
+
+retriever = BM25Retriever(document_store=document_store)
+
+reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
+
+
+pipe = ExtractiveQAPipeline(reader, retriever)
+
+prediction = pipe.run(
+    query="Who is the father of Arya Stark?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+)
 
 #Start the app
 app = Flask(__name__)
