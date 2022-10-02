0cacf53a8869f59b560e67774a2c04d9f9eac1f2
unknown <arnauldkayonga1@gmail.com>
2022-10-02 21:01:35 +0200

    disable gpu

--- main.py
+++ main.py
@@ -58,31 +58,31 @@ app.config['SQLALCHEMY_DATABASE_URI'] = config.get('SQLALCHEMY_DATABASE_URI')
 
 
 #INDEX
-@app.get("/")
+@app.route("/")
 def root():
     return 'Welcome to the Ihugure Chatbot API!'
 
 #SPEECH ENABLED QUERY KINYARWANDA
-@app.get("/webhook/pindo")
+@app.route("/webhook/pindo")
 def webhook_pindo() -> dict:
     print(request)
     return True
 
 #SPEECH ENABLED QUERY KINYARWANDA
-@app.post("/query/speech/rw")
+@app.route("/query/speech/rw")
 def query_speech_rw() -> dict:
     speech_file = request.files['query']  #RW AUDIOFILE
     text_query = stt.convert.to_text(speech_file) #SPEECH TO TEXT CONVERSION
     return {"Query": text_query}
 
 #SPEECH ENABLED QUERY ENGLISH
-@app.post("/query/speech/en")
+@app.route("/query/speech/en")
 def query_speech_en() -> dict:
     speech_file = request.files['query']  #ENG AUDIOFILE
     return {"Query": 'English'}
 
 #TEXT BASED QUERY KINYARWANDA
-@app.post("/query/text/rw")
+@app.route("/query/text/rw")
 def query_text_rw() -> dict:
     query_en = translator.to_en(request.args.get("query"))
     return {"Query": query_en,
@@ -90,7 +90,7 @@ def query_text_rw() -> dict:
     }
 
 #TEXT BASED QUERY ENGLISH
-@app.post("/query/text/en")
+@app.route("/query/text/en")
 def query_text_en() -> dict:
     return {"Query": request.args.get("query"),
             "mobile": request.args.get("mobile")
